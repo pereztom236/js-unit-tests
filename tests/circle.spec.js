ff5ebb7a85b8f8eee4f8e21cce772cd50d39bdd3
Marlon Couto <marloncouto199@gmail.com>
2022-10-20 18:18:46 -0300

    Finaliza requisito 5
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,7 +23,7 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     // Teste se circle retorna um objeto.
@@ -32,5 +32,22 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(isNaN)).toBe(undefined);
+
+    expect(typeof circle(1)).toBe('object');
+
+    expect(Object.keys(circle(2)).length).toBe(3);
+
+    expect(circle()).toBe(undefined);
+
+    expect(circle(2).circumference).toBe(12.56);
+
+    expect(circle(3).area).toBeCloseTo(28.26, 2);
+
+    expect(circle(3)).toEqual({
+      radius: 3,
+      area: 28.26,
+      circumference: 18.84,
+    });
   });
 });
