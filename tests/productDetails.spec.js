5637d04eadd3f3d9785d39c58a3ceab3c977d9a2
Marlon Couto <marloncouto199@gmail.com>
2022-10-20 19:30:24 -0300

    Finaliza requisito 6
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,7 +31,7 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.
@@ -39,5 +39,20 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
+    expect(typeof productDetails).toBe('function');
+    
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
+
+    expect(productDetails('Alcool gel', 'Máscara').length).toBe(2);
+    
+    productDetails('Alcool gel', 'Máscara').forEach(element => {
+      expect(typeof element).toBe('object');
+    });
+    
+    expect(productDetails('Alcool gel', 'Máscara')[0]).not.toBe(productDetails('Alcool gel', 'Máscara')[1]);
+    
+    productDetails('Alcool gel', 'Máscara').forEach(element => {
+      expect(element.details.productId.endsWith('123')).toBe(true);
+    });
   });
 });
